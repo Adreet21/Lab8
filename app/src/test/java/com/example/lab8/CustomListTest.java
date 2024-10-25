50449eb9caad18e44381a2a29c31128a31f48580
Syed Shahmeer Rahman <90085351+Adreet21@users.noreply.github.com>
2024-10-25 13:45:29 -0600

    Update CustomListTest.java

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -12,5 +13,57 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+    
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Montreal", "QC"));
 
+        assertEquals(list.getCount(),listSize + 1);
+    }
+
+    @Test
+    void testHasCity() {
+        list = MockCityList();
+        City city = new City("Dhaka", "BD");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+    }
+
+    @Test
+    void testDelete() {
+        list = MockCityList();
+
+        City city = new City("Vancouver", "BC");
+        list.addCity(city);
+
+        assertEquals(1, list.getCount());
+
+        list.delete(city);
+
+        assertEquals(0, list.getCount());
+    }
+
+    @Test
+    void testCountCities() {
+        list = MockCityList();
+
+        City city1 = new City("Montreal", "QC");
+        list.addCity(city1);
+
+        City city2 = new City("Toronto", "ON");
+        list.addCity(city2);
+
+        City city3 = new City("Vancouver", "BC");
+        list.addCity(city3);
+
+        assertEquals(3, list.countCities());
+        
+    }
 }
