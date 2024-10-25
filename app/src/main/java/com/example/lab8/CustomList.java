e1e79a5397ce232527d5bdfe3bd79e62a3e1b5fe
Syed Shahmeer Rahman <90085351+Adreet21@users.noreply.github.com>
2024-10-25 15:52:00 -0600

    Failing test case for count method CustomList.java

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -60,4 +60,8 @@ public class CustomList extends ArrayAdapter<City> {
     public void delete(City city) {
         cities.remove(city);
     }
+
+    public int countCities() {
+        return 0;
+    }
 }
